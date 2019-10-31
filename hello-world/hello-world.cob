f123dfbace4fdb54c3126f483f0fef099b009db5
Carlos Alba <soradev4@gmail.com>
2019-10-31 13:03:11 -0700

    Create hello-world.cob

---
+++ hello-world/hello-world.cob
@@ -0,0 +1,8 @@
+     ---- hello-world.cob -------------------------
+           * Sample COBOL program
+            IDENTIFICATION DIVISION.
+            PROGRAM-ID. hello.
+            PROCEDURE DIVISION.
+            DISPLAY "Hello World!".
+            STOP RUN.
+     ----------------------------------------
